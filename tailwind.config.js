c4975e7bb5a7e04031f6c5953cf437cdd92eeaee
mgsainyinyitun <mgsainyinyitun.tumdy@gmail.com>
2022-04-13 00:07:36 +0630

    First Commit

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  content: ["./public/**/*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily:{
+        custombyme:["Nunito"],
+      },
+      colors:{
+        primary:'#FF6363',
+        secondary:{
+          100:"#E2E2D5",
+          200:"#888883"
+        },
+      }
+    },
+  },
+  plugins: [],
+}
